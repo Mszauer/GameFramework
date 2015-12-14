32c0cf093ff9adb45103b728587a7e4807f70e8f
mszauer <mszauer@mail.usf.edu>
2015-12-13 16:25:12 -0800

    finished gameobject class

---
+++ 2DFramework/Patterns/Component.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework {
+    class Component {
+        GameObject gameObject = null;
+        public string Name = null;
+        public bool Active = false;
+        public virtual void Initialize(string name, GameObject game) {
+            Name = name;
+            gameObject = game;
+        }
+        public virtual void OnActivate() {
+
+        }
+        public virtual void OnDeactivate() {
+
+        }
+        public virtual void Update(float dTime) {
+
+        }
+        public virtual void Render() {
+
+        }
+        public virtual void Shutdown() {
+            Name = null;
+            gameObject = null;
+            Active = false;
+        }
+    }
+}
---
+++ 2DFramework/Patterns/GameObject.cs
@@ -0,0 +1,157 @@
+﻿
+#define CHILDDEBUG
+#define COMPONENTDEBUG
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GameFramework {
+    class GameObject {
+        public string Name = null;
+        public Point LocalPosition = new Point(0, 0);
+        private bool _enabled = false;
+        public bool Enabled {
+            get {
+                return _enabled;
+            }
+            set {
+                if (value != _enabled) {
+                    if (value) {
+                        OnEnable();
+                    }
+                    else {
+                        OnDisable();
+                    }
+                }
+                _enabled = value;
+            }
+        }
+        public List<GameObject> Children = null;
+        public GameObject Parent = null;
+        public List<Component> Components = null;
+        public void Initialize(string name, Point pos, bool enabled, GameObject parent) {
+            Name = name;
+            LocalPosition = pos;
+            Enabled = enabled;
+            Parent = parent;
+        }
+        public void Update(float dTime) {
+            if (Enabled) {
+                //do self update stuff here
+                if (Children != null) {
+                    for (int i = Children.Count - 1; i >= 0; i--) {
+                        Children[i].Update(dTime);
+                    }
+                }
+            }
+            
+        }
+        public void Render() {
+            if (Enabled) {
+                //do self render stuff here
+                if (Children != null) {
+                    for (int i = Children.Count - 1; i >= 0; i--) {
+                        Children[i].Render();
+                    }
+                }
+            }
+        }
+        public void Shutdown() {
+            Children = null;
+            Components = null;
+            Name = null;
+            LocalPosition = new Point(0, 0);
+            Enabled = false;
+            Parent = null;
+        }
+        public void OnEnable() {
+
+        }
+        public void OnDisable() {
+
+        }
+        public void AddChild(GameObject child) {
+            if (Children == null) {
+                Children = new List<GameObject>();
+            }
+            Children.Add(child);
+#if CHILDDEBUG
+            Console.WriteLine("Added child:" + child.ToString());
+            Console.WriteLine("Children Length: " + Children.Count);
+#endif
+        }
+        public void RemoveChild(GameObject child) {
+            for (int i = Children.Count - 1; i >= 0; i--) {
+                if (Children[i] == child) {
+                    Children.RemoveAt(i);
+#if CHILDDEBUG
+                    Console.WriteLine("Removed child:" + child.ToString() + " at: " + i);
+                    Console.WriteLine("Children Length: " + Children.Count);
+#endif
+                }
+            }
+        }
+
+        public GameObject FindChild(string child) {
+            if (Children == null) {
+#if CHILDDEBUG
+                Console.WriteLine("Child not found");
+#endif
+                return null;
+            }
+            for (int i = Children.Count-1; i >= 0; i--) {
+                if (Children[i].Name == child) {
+#if CHILDDEBUG
+                    Console.WriteLine("Child found at: " + i);
+#endif
+                    return Children[i];
+                }
+                else {
+                    Children[i].FindChild(child);
+                }
+            }
+#if CHILDDEBUG
+            Console.WriteLine("Child not found");
+#endif
+            return null;
+        }
+        public void AddComponent(Component component) {
+            if (Components == null) {
+                Components = new List<Component>();
+            }
+            Components.Add(component);
+#if COMPONENTDEBUG
+            Console.WriteLine("Component added: " + component);
+            Console.WriteLine("Component Length: " + Components.Count);
+#endif
+        }
+        public void RemoveComponent(Component component) {
+            for (int i = Components.Count - 1; i >= 0; i--) {
+                if (Components[i] == component) {
+                    Components.RemoveAt(i);
+#if COMPONENTDEBUG
+                    Console.WriteLine("Component removed: " + component);
+                    Console.WriteLine("Component Length: " + Components.Count);
+#endif
+                }
+            }
+        }
+        public Component FindComponent(String component) {
+            for (int i = Components.Count - 1; i >= 0; i--) {
+                if (Components[i].Name == component) {
+#if COMPONENTDEBUG
+                    Console.WriteLine("Component Found: " + component + " at: " + i);
+#endif
+                    return Components[i];
+                }
+            }
+#if COMPONENTDEBUG
+            Console.WriteLine("Component not found");
+#endif
+            return null;
+        }
+    }
+}
